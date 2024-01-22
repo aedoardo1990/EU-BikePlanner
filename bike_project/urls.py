26eeb912b5ea3636feff0eb1e6b89bd3791e9e51
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-22 09:25:40 +0000

    rename hello_bike to bike_planner

--- bike_project/urls.py
+++ bike_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_bike import views as index_views
+from bike_planner import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
