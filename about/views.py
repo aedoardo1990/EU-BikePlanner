6cf0530d4a2de8b639b8058a8c1794a1e9abb7cc
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-22 09:55:38 +0000

    create about app, aboutPage function and connect to url.py

--- about/views.py
+++ about/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello bike!")
+def aboutPage(request):
+    return HttpResponse("This is the about page!")
