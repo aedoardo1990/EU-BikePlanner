9e490ce0c6f47a50be97079a141a8e918d9ebb87
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-22 14:15:15 +0000

    connect secret key between settings and env.pz

--- bike_project/settings.py
+++ bike_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hke11&r+ps4xayv6$p4(nei!q*s7=l1ncea8hezsq-icz@%bvt'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
