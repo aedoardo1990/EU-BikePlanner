241556b2717f9180c32d1ec97e7bc7b8581ddf7d
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-22 14:54:06 +0000

    create contact model in about.models.py

--- about/models.py
+++ about/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Contact Model inspired from https://github.com/WojtekKamilowski/CI_PP4_MPN/tree/main
+class Contact(models.Model):
+    """
+    Contact model class for Contact form
+    """
+    message_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=200, default="")
+    message = models.TextField()
+    sent_on = models.DateTimeField(auto_add_now=True)
+
+    class Meta:
+        ordering = ["name"]
+    
+    def __str__(self):
+        return self.name
