c3b4dd28551b9e9e32bcfcabcbec258d47f6c0ca
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-23 11:04:59 +0000

    add django summernote and class ContactAdmin in about admin.py

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Contact
+from django_summernote.admin import SummernoteModelAdmin
 
 
-# Register your models here.
-admin.site.register(Contact)
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_filter = ("name", "subject", "email", "sent_on")
+    list_display = ("message_id", "name", "message", "sent_on")
+    search_fields = ["email", "user"]
