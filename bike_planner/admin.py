ef789e14f60bf9e3d4fd16da46024932a63eecf1
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-25 09:17:21 +0000

    add Route model

--- bike_planner/admin.py
+++ bike_planner/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Route
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+admin.site.register(Route)
--- bike_planner/models.py
+++ bike_planner/models.py
@@ -2,4 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
+class Route(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+
