f3099cf96b7f798045c6a1bec496c16ec2352a06
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-01-25 21:13:28 +0000

    delete and makemigrations again for route model to fix admin issue

--- bike_planner/models.py
+++ bike_planner/models.py
@@ -5,18 +5,18 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Route model for creating posts about EU Routes in Admin
-# class Route(models.Model):
-#   title = models.CharField(max_length=200, unique=True)
-#    author = models.ForeignKey(
-#        User,
-#        on_delete=models.CASCADE,
-#        related_name="routes"
-#    )
-#    slug = models.SlugField(max_length=200, unique=True)
-#    content = models.TextField()
-#    status = models.IntegerField(choices=STATUS, default=0)
-#    created_on = models.DateTimeField(auto_now_add=True)
-#    excerpt = models.TextField(blank=True)
+class Route(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
 
 
 
