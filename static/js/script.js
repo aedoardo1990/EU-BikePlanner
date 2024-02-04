80001521dc3a0a68b1c3ff17b97b0618eead7f6d
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-02-04 20:57:37 +0000

    add comment to script.js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+/*Fade effect flash messages - Credits to https://stackoverflow.com/questions/31176402/how-to-hide-flash-message-after-few-seconds*/
 $(document).ready(function(){
     setTimeout(function() {
         $('#alert').fadeOut('slow');
