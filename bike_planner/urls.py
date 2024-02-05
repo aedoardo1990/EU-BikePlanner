856eac049cd5fee6abe8da198b1cb25efe3ae634
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-02-05 21:49:33 +0000

    save changes

--- bike_planner/urls.py
+++ bike_planner/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('contact/', views.ContactDelivered.as_view(), name="contact-us"),
     path('add-trip/', views.AddTrip.as_view(), name="add-trip"),
     path('my-trips/', views.MyTrips.as_view(), name="mytrips"),
-    path('my-trips/<slug:slug>', views.)
     ]
