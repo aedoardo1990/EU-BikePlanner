64f3b883e9bcc806befa46764283f840b76813c4
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-02-15 10:13:06 +0000

    bug when deploy set to false. add cloudinary to settings as I forgot to do it

--- bike_project/settings.py
+++ bike_project/settings.py
@@ -148,7 +148,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
