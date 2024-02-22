f1f459c0063ead79bf5803b9130565297e023ef9
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-02-22 17:48:13 +0000

    add comment on top o script.js to remove validation error

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+/*globals $:false */
+
 /*Fade effect flash messages - Credits to https://stackoverflow.com/questions/31176402/how-to-hide-flash-message-after-few-seconds*/
 $(document).ready(function(){
     setTimeout(function() {
